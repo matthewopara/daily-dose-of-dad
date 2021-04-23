f35c3bbc730c06614b92474ae58f09dd493c21a5
Matthew <m.opara25@gmail.com>
2021-04-22 21:01:36 -0700

    start saved jokes page

---
+++ saved_jokes.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/saved_jokes.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <title>Saved Jokes</title>
+</head>
+<body>
+    <?php include 'navbar.php'; ?>
+
+    <h1>Saved Jokes</h1>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-6 col-md-4">
+                <div class="row">
+                    <div class="col-1 col-sm-2 col-xl-1">
+                        <input class="sound-button" type="image" src="images/icon_volume.png" />
+                    </div>
+                    <div class="col">
+                        Have you heard about the chocolate record player? It sounds pretty sweet.
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4">
+                <div class="row">
+                    <div class="col-1 col-sm-2 col-xl-1">
+                        <input class="sound-button" type="image" src="images/icon_volume.png" />
+                    </div>
+                    <div class="col">
+                        Have you heard about the chocolate record player? It sounds pretty sweet.
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4">
+                <div class="row">
+                    <div class="col-1 col-sm-2 col-xl-1">
+                        <input class="sound-button" type="image" src="images/icon_volume.png" />
+                    </div>
+                    <div class="col">
+                        Have you heard about the chocolate record player? It sounds pretty sweet.
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4">
+                <div class="row">
+                    <div class="col-1 col-sm-2 col-xl-1">
+                        <input class="sound-button" type="image" src="images/icon_volume.png" />
+                    </div>
+                    <div class="col">
+                        Have you heard about the chocolate record player? It sounds pretty sweet.
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4">
+                <div class="row">
+                    <div class="col-1 col-sm-2 col-xl-1">
+                        <input class="sound-button" type="image" src="images/icon_volume.png" />
+                    </div>
+                    <div class="col">
+                        Have you heard about the chocolate record player? It sounds pretty sweet.
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4">
+                <div class="row">
+                    <div class="col-1 col-sm-2 col-xl-1">
+                        <input class="sound-button" type="image" src="images/icon_volume.png" />
+                    </div>
+                    <div class="col">
+                        Have you heard about the chocolate record player? It sounds pretty sweet.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+</body>
+</html>
