ad2d4b96d194cc284a0b7c77a7b6a7c5b595cdce
Matthew <m.opara25@gmail.com>
2021-04-22 19:24:04 -0700

    fixed indentation

--- navbar.php
+++ navbar.php
@@ -1,21 +1,21 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="index.php">Daily Dose of Dad</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <a class="nav-link active" aria-current="page" href="index.php">Dad Jokes</a>
-        <a class="nav-link" href="#">Saved Jokes</a>
-        <div class="dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Account
-              </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <li><a class="dropdown-item" href="login.php">Login</a></li>
-                <li><a class="dropdown-item" href="sign_up.php">Sign Up</a></li>
-              </ul>
-        </div>
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">Daily Dose of Dad</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <a class="nav-link active" aria-current="page" href="index.php">Dad Jokes</a>
+      <a class="nav-link" href="saved_jokes.php">Saved Jokes</a>
+      <div class="dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Account
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <li><a class="dropdown-item" href="login.php">Login</a></li>
+              <li><a class="dropdown-item" href="sign_up.php">Sign Up</a></li>
+            </ul>
       </div>
     </div>
-  </nav>
+  </div>
+</nav>
