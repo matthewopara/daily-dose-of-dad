e9e976a256d40b031966e51b4e2a41c3875e9452
Matthew <m.opara25@gmail.com>
2021-04-24 13:47:34 -0700

    change navbar colors

--- navbar.php
+++ navbar.php
@@ -1,14 +1,14 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg" style="background-color: #bad7f2;">
   <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">Daily Dose of Dad</a>
+    <a class="navbar-brand" href="index.php" style="color: #003796;">Daily Dose of Dad</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-      <a class="nav-link active" aria-current="page" href="index.php">Dad Jokes</a>
-      <a class="nav-link" href="saved_jokes.php">Saved Jokes</a>
+      <a class="nav-link active" aria-current="page" href="index.php" style="color: #003796;">Dad Jokes</a>
+      <a class="nav-link" href="saved_jokes.php" style="color: #003796;">Saved Jokes</a>
       <div class="dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a style="color: #003796;" class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Account
             </a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
