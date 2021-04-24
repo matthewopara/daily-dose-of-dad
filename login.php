aec503e0e38f9a852f9c695caa56fbc0f7c5fc57
Matthew <m.opara25@gmail.com>
2021-04-24 13:52:17 -0700

    set background color for all pages

--- login.php
+++ login.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/default.css">
     <link rel="stylesheet" href="styles/form.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <title>Login</title>
@@ -11,7 +12,7 @@
 <body>
     <?php include 'navbar.php'; ?>
 
-    <div>
+    <div id="background">
         <h1 id="sign-up">Login</h1>
         <form id="form">
             <label for="user" class="form-label">Username:</label><br>
